79fac1dab0625e1b28fc29996fa6e1411636a6c5
Mar Cabrera <mdmcabreracast@gmail.com>
2022-05-15 17:21:45 +0100

    Remove favorite button on cell

--- aries/aries/LaunchCell.swift
+++ aries/aries/LaunchCell.swift
@@ -11,19 +11,12 @@ import UIKit
 class LaunchCell: UITableViewCell {
     // MARK: - IBOutlets
     @IBOutlet weak var nameLabel: UILabel!
-    @IBOutlet weak var favoriteButton: UIButton!
     
     // MARK: - Properties
-    var isFavorite: Bool = false
     
     // MARK: - Methods
     func setup(model: Launch) {
         nameLabel.text = model.name
-        
-    }
-    
-    @IBAction func favoriteButtonTapped(_ sender: Any) {
-        favoriteButton.isSelected ? favoriteButton.setBackgroundImage(UIImage(named: "star"), for: .selected) : favoriteButton.setBackgroundImage(UIImage(named: "start.fill"), for: .normal)
     }
     
     
