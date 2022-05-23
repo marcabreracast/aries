f3f357c553b4d9b1c84921b0e83ba40a16485c64
Mar Cabrera <mar.cabrera@mongodb.com>
2022-05-23 17:18:11 +0100

    Add details property to Launch model

--- aries/aries/Models/Launch.swift
+++ aries/aries/Models/Launch.swift
@@ -9,11 +9,13 @@ import Foundation
 
 struct Launch: Codable {
     let name: String
+    let details: String?
     let dateUnix: Double
     let upcoming: Bool
 
     enum CodingKeys: String, CodingKey {
         case name
+        case details
         case dateUnix = "date_unix"
         case upcoming
     }
