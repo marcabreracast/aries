f3b8a20de94aa934cf0d6130018337b79f140cf9
Mar Cabrera <mar.cabrera@mongodb.com>
2022-05-25 12:40:09 +0100

    Add Realm Package + Basic Auth implementation for Email/Password

---
+++ aries/aries/LoginViewController.swift
@@ -0,0 +1,63 @@
+//
+//  LoginViewController.swift
+//  aries
+//
+//  Created by Mar Cabrera on 25/05/2022.
+//
+
+import UIKit
+import RealmSwift
+
+class LoginViewController: UIViewController {
+    // MARK: - IBOutlets
+    @IBOutlet weak var emailTextfield: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    // MARK: - View Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        emailTextfield.placeholder = "email@example.com"
+        passwordTextField.placeholder = "Password"
+        passwordTextField.isSecureTextEntry = true
+    }
+    
+    
+    // MARK: - IBActions
+    
+    @IBAction func loginButtonTapped(_ sender: Any) {
+        let email = "skroob@example.com"
+        let password = "1234567"
+       // let email = emailTextfield.text
+       // let password = passwordTextField.text
+        app.login(credentials: Credentials.emailPassword(email: email, password: password)) { (result) in
+            DispatchQueue.main.async {
+                switch result {
+                case .failure(let error):
+                    print("Login failed: \(error.localizedDescription)")
+                    
+                    
+                case .success(let user):
+                    print("Successfully logged in as user \(user)")
+                    // Now logged in, do something with user
+                    // Remember to dispatch to main if you are doing anything on the UI thread
+                    self.performSegue(withIdentifier: "goToLaunches", sender: nil)
+                }
+
+            }
+        }
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
