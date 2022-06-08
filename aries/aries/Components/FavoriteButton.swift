4670a5a8fd0effcf91d8308a5b5cf9844d3f66be
Mar Cabrera <mar.cabrera@mongodb.com>
2022-06-08 11:43:48 +0100

    Add custom Favorite Button

---
+++ aries/aries/Components/FavoriteButton.swift
@@ -0,0 +1,36 @@
+//
+//  FavoriteButton.swift
+//  aries
+//
+//  Created by Mar Cabrera on 08/06/2022.
+//
+
+import Foundation
+import UIKit
+
+class FavoriteButton: UIButton {
+    // MARK: - Properties
+    private let unfavoritedImage = UIImage(systemName: "star")
+    private let favoritedImage = UIImage(systemName: "star.fill")
+    private var isFavorite = false
+
+    // MARK: - Public Functions
+    func flipFavoritedState() {
+        isFavorite = !isFavorite
+        animate()
+    }
+
+    // MARK: - Private Helpers
+    private func animate() {
+        UIView.animate(withDuration: 0.1, animations: {
+            let newImage = self.isFavorite ? self.favoritedImage : self.unfavoritedImage
+            self.transform = self.transform.scaledBy(x: 0.8, y: 0.8)
+            self.setImage(newImage, for: .normal)
+q
+        }, completion: { _ in
+            UIView.animate(withDuration: 0.1, animations: {
+                self.transform = CGAffineTransform.identity
+            })
+        })
+    }
+}
