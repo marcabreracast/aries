7381f5848ccafda03af5a4842d33624e12d254bc
Mar Cabrera <mar.cabrera@mongodb.com>
2022-06-10 10:35:31 +0100

    Add list of favorite launches for the user

--- aries/aries/AccountViewController.swift
+++ aries/aries/AccountViewController.swift
@@ -11,12 +11,27 @@ import RealmSwift
 class AccountViewController: UIViewController {
     // MARK: - IBOutlets
     @IBOutlet weak var logoutButton: LoadingButton!
+    @IBOutlet weak var tableView: UITableView!
+
+    // MARK: - Properties
+    let realm = try! Realm()
+    var user: User?
+    lazy var favoriteLaunches: List<UserLaunches>? = nil
 
     // MARK: - View Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.navigationItem.setHidesBackButton(true, animated: true)
+
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.separatorColor = .white
+
+        // Fetch favorite launches from the database
+        user = realm.objects(User.self).first
+
+        favoriteLaunches = user?.launches
     }
 
     // MARK: - IBActions
@@ -42,4 +57,28 @@ class AccountViewController: UIViewController {
     }
     */
 
+}
+
+// MARK: - TableView Delegate
+extension AccountViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        let cell = tableView.dequeueReusableCell(withIdentifier: "favoriteLaunchCell", for: indexPath) as! FavoriteLaunchCell
+
+        cell.title.text = favoriteLaunches?[indexPath.row].name
+        cell.selectionStyle = .none
+
+        return cell
+    }
+}
+
+// MARK: - TableView Data Source
+extension AccountViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return favoriteLaunches?.count ?? 0
+    }
+}
+
+class FavoriteLaunchCell: UITableViewCell {
+    @IBOutlet weak var title: UILabel!
 }
