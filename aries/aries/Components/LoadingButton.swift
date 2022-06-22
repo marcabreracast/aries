382da3b79be75f090c5d60914e24a6cad8f1b832
Mar Cabrera <mar.cabrera@mongodb.com>
2022-06-22 10:33:12 +0100

    Add font to custom button

--- aries/aries/Components/LoadingButton.swift
+++ aries/aries/Components/LoadingButton.swift
@@ -20,6 +20,7 @@ class LoadingButton: UIButton {
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)!
 
+        self.titleLabel?.font = UIFont(name: "Oxanium-Regular", size: 17)
         self.layer.cornerRadius = 5
         self.backgroundColor = #colorLiteral(red: 0.6060286164, green: 0.5469256043, blue: 0.5953031778, alpha: 1)
     }
