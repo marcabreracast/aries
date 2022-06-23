9d10f1d9f98016e0a44c25b9adca1565ff3dd5a9
Mar Cabrera <mar.cabrera@mongodb.com>
2022-06-23 10:05:14 +0100

    Add implementation for fetching Rockets from API and storing them in user launch when he taps favorite

--- aries/aries/Cells/FavoriteLaunchCell.swift
+++ aries/aries/Cells/FavoriteLaunchCell.swift
@@ -9,16 +9,21 @@ import Foundation
 import UIKit
 
 class FavoriteLaunchCell: UICollectionViewCell {
-    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var rocketLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    
 
     func setup(model: UserLaunches) {
-        self.label.text = model.name
+        self.nameLabel.text = model.name
         if let image = model.links?.image {
             self.imageView.setCustomImage(image)
         } else {
             self.imageView.image = UIImage(named: "no-image")
         }
+        dateLabel.text = DateHelper.formatShortUnixDate(date: model.date_unix ?? 0.0)
+        rocketLabel.text = model.rocket
 
     }
 }
