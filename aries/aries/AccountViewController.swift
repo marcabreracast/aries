08ae47c433825d2bb90393e161469be247741f3d
Mar Cabrera <mar.cabrera@mongodb.com>
2022-07-01 12:22:43 +0100

    Replace logout button with tableview to include reset password section

--- aries/aries/AccountViewController.swift
+++ aries/aries/AccountViewController.swift
@@ -8,9 +8,14 @@
 import UIKit
 import RealmSwift
 
+enum CellType: Int, CaseIterable {
+    case resetPassword = 1
+    case logout = 2
+}
+
 class AccountViewController: UIViewController {
     // MARK: - IBOutlets
-    @IBOutlet weak var logoutButton: LoadingButton!
+    @IBOutlet weak var tableView: UITableView!
 
     // MARK: - Properties
     let realm = try! Realm()
@@ -21,21 +26,61 @@ class AccountViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.separatorColor = .white
     }
 
     // MARK: - Private Helpers
 
-
-    // MARK: - IBActions
-    @IBAction func logoutButtonTapped(_ sender: Any) {
-        logoutButton.showLoading()
+    private func logoutUser() {
         app.currentUser?.logOut(){ (_) in
             DispatchQueue.main.async {
                 print("Logged out!")
-                self.logoutButton.hideLoading()
                 self.performSegue(withIdentifier: "goToLoginViewController", sender: nil)
             }
         }
+    }
+}
+
+extension AccountViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return CellType.allCases.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "accountCell", for: indexPath) as! AccountCell
+
+        let cellType = CellType(rawValue: indexPath.row + 1)
+
+        switch cellType {
+        case .resetPassword:
+            cell.setup(title: "Reset Password", isIconPresent: true)
+        case .logout:
+            cell.setup(title: "Logout", isIconPresent: false)
+        case .none:
+            break
+        }
+
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let cellType = CellType(rawValue: indexPath.row + 1)
+
+        switch cellType {
+        case .resetPassword:
+            // TODO
+            print("Reset password")
+        case .logout:
+            logoutUser()
+        case .none:
+            break
+        }
+    }
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
     }
 }
 
---
+++ aries/aries/Cells/AccountCell.swift
@@ -0,0 +1,33 @@
+//
+//  AccountCell.swift
+//  aries
+//
+//  Created by Mar Cabrera on 01/07/2022.
+//
+
+import UIKit
+
+class AccountCell: UITableViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var icon: UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+
+    public func setup(title: String, isIconPresent: Bool) {
+        titleLabel.text = title
+
+        if isIconPresent {
+            icon.image = UIImage(systemName: "chevron.right")
+        } else {
+            icon.isHidden = true
+        }
+    }
+}
