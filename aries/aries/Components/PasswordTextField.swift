19e5015e55db731843e162ac30766f994626ebc2
Mar Cabrera <mar.cabrera@mongodb.com>
2022-07-04 11:28:50 +0100

    Fix padding issue regarding icon

--- aries/aries/Components/PasswordTextField.swift
+++ aries/aries/Components/PasswordTextField.swift
@@ -25,6 +25,7 @@ class PasswordTextField: UITextField {
 
         // Add right icon functionality
         let button = UIButton(frame: CGRect(x: 0, y: 0, width: 30, height: 30))
+        button.tintColor = #colorLiteral(red: 0.9472092986, green: 0.912545681, blue: 0.8959150314, alpha: 1)
         button.setImage(UIImage(systemName: "eye"), for: .normal)
         button.setImage(UIImage(systemName: "eye.slash"), for: .selected)
         self.rightView = button
@@ -35,5 +36,15 @@ class PasswordTextField: UITextField {
     @objc private func showHidePassword(_ sender: UIButton) {
         sender.isSelected = !sender.isSelected
         self.isSecureTextEntry = !sender.isSelected
+    }
+
+    override func rightViewRect(forBounds bounds: CGRect) -> CGRect {
+        let offset = 9
+        let width  = 30
+        let height = width
+        let x = Int(bounds.width) - width - offset
+        let y = offset
+        let rightViewBounds = CGRect(x: x, y: y, width: width, height: height)
+        return rightViewBounds
     }
 }
