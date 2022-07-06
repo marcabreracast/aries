a205bc4ec5029bee7edd1ec8cb41a697d0178b33
Mar Cabrera <mar.cabrera@mongodb.com>
2022-07-06 16:36:03 +0100

    Add Universal Links functionality to link it to hosting website

--- aries/aries/SceneDelegate.swift
+++ aries/aries/SceneDelegate.swift
@@ -32,6 +32,15 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         }
     }
 
+    // UNIVERSAL LINKS HANDLING
+    func scene(_ scene: UIScene, continue userActivity: NSUserActivity) {
+
+        guard let url = userActivity.webpageURL else { return }
+
+        print(url)
+
+    }
+
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
         // This occurs shortly after the scene enters the background, or when its session is discarded.
@@ -59,7 +68,5 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
     }
-
-
 }
 
