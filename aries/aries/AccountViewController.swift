f83a161ea9fae62d47baa8c70a15918152fbcd6e
Mar Cabrera <mar.cabrera@mongodb.com>
2022-07-11 15:53:12 +0100

    Add Check your email screen once reset password email is sent

--- aries/aries/AccountViewController.swift
+++ aries/aries/AccountViewController.swift
@@ -47,20 +47,16 @@ class AccountViewController: UIViewController {
         let client = app.emailPasswordAuth
 
         client.sendResetPasswordEmail(email) { (error) in
+            // There should be something to show loading
             DispatchQueue.main.async {
                 guard error == nil else {
                     print("Reset password email not sent: \(error!.localizedDescription)")
                     return
                 }
 
-                print("Password reset email sent tp the following address: \(email)")
+                print("Password reset email sent to the following address: \(email)")
 
-                // Present alert to notify user
-                let alert = UIAlertController(title: "Reset Password", message: "An email has been sent. Please check your inbox and follow the next steps!", preferredStyle: UIAlertController.Style.alert)
-
-                alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
-
-                self.present(alert, animated: true, completion: nil)
+                self.performSegue(withIdentifier: "goToCheckYourEmailVC", sender: nil)
             }
         }
     }
